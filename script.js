5ef0b20dc3c0859703a629c23454e4f5f1e38537
zweidmitr <zweidmitr@gmail.com>
2022-09-02 08:20:39 +0300

    dataTypes

---
+++ script.js
@@ -0,0 +1,17 @@
+true
+let javascriptIsFun = true
+console.log(javascriptIsFun)
+
+console.log(typeof true)
+console.log(typeof 'str')
+console.log(typeof 31)
+
+let year
+console.log(year)
+console.log(typeof year)
+
+year = 1991
+console.log(year)
+console.log(typeof year)
+
+console.log(null)
