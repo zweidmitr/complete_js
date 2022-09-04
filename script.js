fef67fee28c91232eaccd087a1370cfa7171293d
zweidmitr <zweidmitr@gmail.com>
2022-09-04 16:46:12 +0300

    functions

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991)
 
 console.log(age1, age2)
-*/
+
 
 // arrow function
 const calcAge3 = (birthYear) => 2037 - birthYear
@@ -57,3 +57,38 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, 'd'))
 console.log(yearsUntilRetirement(2003, 'v'))
+
+function cutFruitPieces(fruit) {
+  return fruit * 4
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples)
+  const orangePieces = cutFruitPieces(oranges)
+
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`
+  return juice
+}
+
+console.log(fruitProcessor(2, 3))
+*/
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear)
+  const retirement = 65 - age
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`)
+    return retirement
+  } else {
+    console.log(`${firstName} has already retired`)
+    return -1
+  }
+}
+
+console.log(yearsUntilRetirement(1991, 'dmitrii'))
+console.log(yearsUntilRetirement(1950, 'mike'))
