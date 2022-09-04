a7fccce2441ff38b172da008bee87e580344eeab
zweidmitr <zweidmitr@gmail.com>
2022-09-04 15:32:42 +0300

    arrow / declaration /expression function

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ if (hasDriversLicense) console.log('I can drive')
 
 // const interface = 'Audio'
 // const private 534
-*/
 
 function logger() {
   console.log('My name Dmitrii')
@@ -28,3 +27,33 @@ console.log(appleJuice)
 
 const appleOrangeJuice = fruitProcessor(2, 4)
 console.log(appleOrangeJuice)
+
+
+//function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear
+}
+const age1 = calcAge1(1991)
+
+// function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear
+}
+const age2 = calcAge2(1991)
+
+console.log(age1, age2)
+*/
+
+// arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear
+console.log(calcAge3(1991))
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2022 - birthYear
+  const retirement = 65 - age
+  // return retirement
+  return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(1991, 'd'))
+console.log(yearsUntilRetirement(2003, 'v'))
