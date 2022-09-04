4083c4112253b6e1f75e703257b915be438b7564
zweidmitr <zweidmitr@gmail.com>
2022-09-04 18:38:22 +0300

    challenge

--- script.js
+++ script.js
@@ -71,7 +71,6 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 3))
-*/
 
 const calcAge = function (birthYear) {
   return 2037 - birthYear
@@ -92,3 +91,31 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(1991, 'dmitrii'))
 console.log(yearsUntilRetirement(1950, 'mike'))
+
+*/
+
+const calcAverage = (a, b, c) => (a + b + c) / 3
+
+let scoreDolphins = calcAverage(44, 23, 71)
+let scoreKoalas = calcAverage(65, 54, 49)
+
+console.log(scoreDolphins)
+console.log(scoreKoalas)
+
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`)
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`)
+  } else {
+    console.log(`no team wins...`)
+  }
+}
+checkWinner(scoreDolphins, scoreKoalas)
+
+scoreDolphins = calcAverage(85, 54, 41)
+scoreKoalas = calcAverage(23, 34, 27)
+
+console.log(scoreDolphins)
+console.log(scoreKoalas)
+checkWinner(scoreDolphins, scoreKoalas)
