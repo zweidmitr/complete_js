13e219195f670d4d93a17dd0f30ed50b1c62f5c2
zweidmitr <zweidmitr@gmail.com>
2022-09-06 00:08:09 +0300

    the end part 2

--- script.js
+++ script.js
@@ -241,7 +241,6 @@ console.log(
   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
 )
 
-*/
 
 const jonas = {
   firstName: 'Jonas',
@@ -279,3 +278,163 @@ console.log(jonas.age)
 console.log(jonas.age)
 
 console.log(jonas.getSummary())
+
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2
+    return this.bmi
+  },
+}
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2
+    return this.bmi
+  },
+}
+
+mark.calcBMI()
+john.calcBMI()
+
+if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI(${john.bmi} is higer than ${mark.fullName}'s (${mark.bmi})!)`
+  )
+} else if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI(${mark.bmi} is higer than ${john.fullName}'s (${john.bmi})!)`
+  )
+}
+
+// console.log('Lifting weghts reperirion 0 (⌐■_■)')
+// console.log('Lifting weghts reperirion 1 (⌐■_■)')
+// console.log('Lifting weghts reperirion 2 (⌐■_■)')
+// console.log('Lifting weghts reperirion 3 (⌐■_■)')
+// console.log('Lifting weghts reperirion 4 (⌐■_■)')
+// console.log('Lifting weghts reperirion 5 (⌐■_■)')
+// console.log('Lifting weghts reperirion 6 (⌐■_■)')
+// console.log('Lifting weghts reperirion 7 (⌐■_■)')
+// console.log('Lifting weghts reperirion 8 (⌐■_■)')
+// console.log('Lifting weghts reperirion 9 (⌐■_■)')
+
+// for loop keeps running while condition is TRUE
+for (let rep = 0; rep < 10; rep++) {
+  console.log(`Lifting weghts reperirion ${rep} (⌐■_■)`)
+}
+
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Steven', 'Peter'],
+  true,
+]
+
+const types = []
+
+for (let i = 0; i < jonas.length; i++) {
+  // reading from jonas array
+  console.log(jonas[i])
+
+  // filling types array
+  // types[i] = typeof jonas[i]
+  types.push(typeof jonas[i])
+}
+
+console.log(types)
+
+const years = [1991, 2007, 1969, 2020]
+const ages = []
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i])
+}
+console.log(ages)
+
+// continue and break
+console.log('--- ONLY STRINGS ---')
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== 'string') continue
+  console.log(jonas[i])
+}
+
+console.log('--- BREAK WITH NUMBER ---')
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === 'number') break
+  console.log(jonas[i])
+}
+
+
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Steven', 'Peter'],
+]
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(jonas[i])
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-- Starting exercise ${exercise}`)
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} ^_^`)
+  }
+}
+
+
+for (let rep = 1; rep < 10; rep++) {
+  // console.log(`Lifting weghts reperirion ${rep} (⌐■_■)`)
+}
+
+let rep = 1
+while (rep <= 10) {
+  // console.log(`Lifting weghts reperirion ${rep} (⌐■_■)`)
+  rep++
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1
+// console.log(dice)
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`)
+  dice = Math.trunc(Math.random() * 6) + 1
+  if (dice === 6) console.log('Loop is about to end...')
+}
+
+*/
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+const tips = []
+const totals = []
+function calcTip(bill) {
+  return bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill
+}
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]))
+  totals.push(tips[i] + bills[i])
+}
+
+console.log(bills)
+console.log(tips)
+console.log(totals)
+
+function calcAverage(arr) {
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i]
+  }
+  return sum / arr.length
+}
+let a = calcAverage(bills)
+let b = calcAverage(tips)
+let c = calcAverage(totals)
+
+console.log(a, b, c, a + b - c)
