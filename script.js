b4969b6537173d995f676510735b67dcdec20f79
zweidmitr <zweidmitr@gmail.com>
2022-09-05 10:22:58 +0300

    array - start

--- script.js
+++ script.js
@@ -92,7 +92,6 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 console.log(yearsUntilRetirement(1991, 'dmitrii'))
 console.log(yearsUntilRetirement(1950, 'mike'))
 
-*/
 
 const calcAverage = (a, b, c) => (a + b + c) / 3
 
@@ -119,3 +118,87 @@ scoreKoalas = calcAverage(23, 34, 27)
 console.log(scoreDolphins)
 console.log(scoreKoalas)
 checkWinner(scoreDolphins, scoreKoalas)
+
+const friend1 = 'Michael'
+const friend2 = 'Steven'
+const friend3 = 'Peter'
+
+const friends = ['Michael', 'Steven', 'Peter']
+console.log(friends)
+
+// const years = new Array(1991, 1984, 2008, 2020)
+
+console.log(friends[0])
+console.log(friends[2])
+
+console.log(friends.length)
+console.log(friends[friends.length - 1])
+
+friends[2] = 'Jay'
+console.log(friends)
+
+const firstName = 'Jonas'
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends]
+console.log(jonas)
+console.log(jonas.length)
+
+// Exercise
+function calcAge(birthYear) {
+  return 2037 - birthYear
+}
+const years = [1990, 1967, 2002, 2010, 2018]
+
+const age1 = calcAge(years[0])
+const age2 = calcAge(years[1])
+const age3 = calcAge(years[2])
+console.log(age1, age2, age3)
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2])]
+console.log(ages)
+
+const friends = ['Michael', 'Steven', 'Peter']
+
+//Add elements
+const newLength = friends.push('Jay')
+console.log(friends)
+console.log(newLength)
+
+friends.unshift('John')
+console.log(friends)
+
+//Remove elements
+friends.pop() // last
+const popped = friends.pop() // last
+console.log(popped)
+console.log(friends)
+
+friends.shift()
+console.log(friends)
+
+console.log(friends.indexOf('Steven'))
+console.log(friends.indexOf('Bob'))
+
+friends.push(23)
+friends.push('Peter')
+console.log(friends.includes('Steven'))
+console.log(friends.includes(23))
+
+if (friends.includes('Peter')) {
+  console.log('you have a friend called Peter')
+}
+
+*/
+
+function calcTip(bill) {
+  return bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill
+}
+
+const bills = [125, 555, 44]
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+console.log(bills, tips)
+
+const total = []
+total.push(bills[0] + tips[0])
+total.push(bills[1] + tips[1])
+total.push(bills[2] + tips[2])
+console.log(total)
