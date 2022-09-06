ad3c723edef5c3514d17ff44b68e6661a309efde
zweidmitr <zweidmitr@gmail.com>
2022-09-06 13:36:54 +0300

    Developer Skills & Editor Setup

--- script.js
+++ script.js
@@ -1,440 +1,117 @@
-'use strict'
+// Remember ue strict mode in all scripts now!
+'use strinct'
 /*
-let hasDriversLicense = false
-const passTest = true
+// PROBLEM 1 "Given an array of temperatures of one day, calculate the temperature amplotude. Keep in min that sometimes there might be a sensor error
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5]
 
-if (passTest) hasDriversLicense = true
-if (hasDriversLicense) console.log('I can drive')
+// 1) Understanding the problem
+// - What is temp amplitude? Answer: difference between highest and lowest temp
+// - How to compute max and min temp?
+// - What's a sensor error? and what do do?
 
-// const interface = 'Audio'
-// const private 534
+// 2) Braking up into sub-problems
+// - How to ignore errors?
+// - Find max valuer in temp array
+// - Find min valuer in temp array
+// - Subtract min from max (amplitude) and return it
 
-function logger() {
-  console.log('My name Dmitrii')
-}
-// calling / running / invoking function
-logger()
-// logger()
-// logger()
-
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`
-  return juice
-}
-
-let appleJuice = fruitProcessor(5, 0)
-console.log(appleJuice)
-
-const appleOrangeJuice = fruitProcessor(2, 4)
-console.log(appleOrangeJuice)
-
-
-//function declaration
-function calcAge1(birthYear) {
-  return 2037 - birthYear
-}
-const age1 = calcAge1(1991)
-
-// function expression
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear
-}
-const age2 = calcAge2(1991)
-
-console.log(age1, age2)
-
-
-// arrow function
-const calcAge3 = (birthYear) => 2037 - birthYear
-console.log(calcAge3(1991))
-
-const yearsUntilRetirement = (birthYear, firstName) => {
-  const age = 2022 - birthYear
-  const retirement = 65 - age
-  // return retirement
-  return `${firstName} retires in ${retirement} years`
-}
-
-console.log(yearsUntilRetirement(1991, 'd'))
-console.log(yearsUntilRetirement(2003, 'v'))
-
-function cutFruitPieces(fruit) {
-  return fruit * 4
-}
-
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples)
-  const orangePieces = cutFruitPieces(oranges)
-
-  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`
-  return juice
-}
-
-console.log(fruitProcessor(2, 3))
-
-const calcAge = function (birthYear) {
-  return 2037 - birthYear
-}
-
-const yearsUntilRetirement = function (birthYear, firstName) {
-  const age = calcAge(birthYear)
-  const retirement = 65 - age
-
-  if (retirement > 0) {
-    console.log(`${firstName} retires in ${retirement} years`)
-    return retirement
-  } else {
-    console.log(`${firstName} has already retired`)
-    return -1
+const calcTempAmplitude = function (temps) {
+  let max = temps[0]
+  let min = temps[0]
+  for (let i = 1; i < temps.length; i++) {
+    const curTemp = temps[i]
+    if (typeof curTemp !== 'number') continue
+    if (curTemp > max) max = curTemp
+    if (curTemp < min) min = curTemp
   }
+  console.log(max, min)
+  return max - min
 }
+const amplitude = calcTempAmplitude(temperatures)
+console.log(amplitude)
 
-console.log(yearsUntilRetirement(1991, 'dmitrii'))
-console.log(yearsUntilRetirement(1950, 'mike'))
+// PROBPEM 2:
+// Function should now receive 2 arrays of temps
+// 1) Understanding the problem
+// - With 2 arrays, should we implements functionality twice? NO! Just merge two arrays
 
+// 2) Braking up into sub-problems
+// - Merge 2 arrays?
 
-const calcAverage = (a, b, c) => (a + b + c) / 3
+const calsTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2)
+  console.log(temps)
 
-let scoreDolphins = calcAverage(44, 23, 71)
-let scoreKoalas = calcAverage(65, 54, 49)
-
-console.log(scoreDolphins)
-console.log(scoreKoalas)
-
-function checkWinner(avgDolphins, avgKoalas) {
-  if (avgDolphins >= 2 * avgKoalas) {
-    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`)
-  } else if (avgKoalas >= 2 * avgDolphins) {
-    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`)
-  } else {
-    console.log(`no team wins...`)
+  let max = temps[0]
+  let min = temps[0]
+  for (let i = 1; i < temps.length; i++) {
+    const curTemp = temps[i]
+    if (typeof curTemp !== 'number') continue
+    if (curTemp > max) max = curTemp
+    if (curTemp < min) min = curTemp
   }
+  console.log(max, min)
+  return max - min
 }
-checkWinner(scoreDolphins, scoreKoalas)
-
-scoreDolphins = calcAverage(85, 54, 41)
-scoreKoalas = calcAverage(23, 34, 27)
-
-console.log(scoreDolphins)
-console.log(scoreKoalas)
-checkWinner(scoreDolphins, scoreKoalas)
-
-const friend1 = 'Michael'
-const friend2 = 'Steven'
-const friend3 = 'Peter'
-
-const friends = ['Michael', 'Steven', 'Peter']
-console.log(friends)
-
-// const years = new Array(1991, 1984, 2008, 2020)
-
-console.log(friends[0])
-console.log(friends[2])
-
-console.log(friends.length)
-console.log(friends[friends.length - 1])
-
-friends[2] = 'Jay'
-console.log(friends)
-
-const firstName = 'Jonas'
-const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends]
-console.log(jonas)
-console.log(jonas.length)
-
-// Exercise
-function calcAge(birthYear) {
-  return 2037 - birthYear
-}
-const years = [1990, 1967, 2002, 2010, 2018]
-
-const age1 = calcAge(years[0])
-const age2 = calcAge(years[1])
-const age3 = calcAge(years[2])
-console.log(age1, age2, age3)
-
-const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2])]
-console.log(ages)
-
-const friends = ['Michael', 'Steven', 'Peter']
-
-//Add elements
-const newLength = friends.push('Jay')
-console.log(friends)
-console.log(newLength)
-
-friends.unshift('John')
-console.log(friends)
-
-//Remove elements
-friends.pop() // last
-const popped = friends.pop() // last
-console.log(popped)
-console.log(friends)
-
-friends.shift()
-console.log(friends)
-
-console.log(friends.indexOf('Steven'))
-console.log(friends.indexOf('Bob'))
-
-friends.push(23)
-friends.push('Peter')
-console.log(friends.includes('Steven'))
-console.log(friends.includes(23))
-
-if (friends.includes('Peter')) {
-  console.log('you have a friend called Peter')
-}
-
-function calcTip(bill) {
-  return bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill
-}
-
-const bills = [125, 555, 44]
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
-console.log(bills, tips)
-
-const total = []
-total.push(bills[0] + tips[0])
-total.push(bills[1] + tips[1])
-total.push(bills[2] + tips[2])
-console.log(total)
+const amplitudeNew = calsTempAmplitudeNew([3, 5, 1], [9, 0, 5])
+console.log(amplitudeNew)
 
 
-const jonas = {
-  firstName: 'Jonas',
-  lastName: 'Schmedtmann',
-  age: 2037 - 1991,
-  job: 'teacher',
-  friends: ['Michael', 'Peter', 'Steven'],
-}
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'cels',
 
-console.log(jonas)
-
-console.log(jonas.lastName)
-console.log(jonas['lastName'])
-
-const nameKey = 'Name'
-console.log(jonas['first' + nameKey])
-console.log(jonas['last' + nameKey])
-
-const inerestedIn = prompt(
-  'What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends'
-)
-
-if (jonas[inerestedIn]) {
-  console.log(jonas[inerestedIn])
-} else {
-  console.log(
-    'Wrong request! Choose between firstName, lastName, age, job, and friends'
-  )
-}
-
-jonas.location = 'Portugal'
-jonas['teitter'] = '@jonasschmedtman'
-console.log(jonas)
-
-// Challenge
-// "Jonas has 3 friends, and his best friend is called Michael"
-console.log(
-  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
-)
-
-
-const jonas = {
-  firstName: 'Jonas',
-  lastName: 'Schmedtmann',
-  birthYear: 1991,
-  job: 'teacher',
-  friends: ['Michael', 'Peter', 'Steven'],
-  hasDriversLicense: false,
-
-  // calcAge: function (birthYear) {
-  //   return 2037 - birthYear
-  // },
-
-  // calcAge: function () {
-  //   // console.log(this)
-  //   return 2037 - this.birthYear
-  // },
-
-  calcAge: function () {
-    this.age = 2037 - this.birthYear
-    return this.age
-  },
-
-  getSummary: function () {
-    return `${this.firstName} is a ${this.calcAge()}-year old ${
-      this.job
-    }, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`
-  },
-}
-
-console.log(jonas.calcAge())
-
-console.log(jonas.age)
-console.log(jonas.age)
-console.log(jonas.age)
-
-console.log(jonas.getSummary())
-
-const mark = {
-  fullName: 'Mark Miller',
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    this.bmi = this.mass / this.height ** 2
-    return this.bmi
-  },
-}
-const john = {
-  fullName: 'John Smith',
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    this.bmi = this.mass / this.height ** 2
-    return this.bmi
-  },
-}
-
-mark.calcBMI()
-john.calcBMI()
-
-if (john.bmi > mark.bmi) {
-  console.log(
-    `${john.fullName}'s BMI(${john.bmi} is higer than ${mark.fullName}'s (${mark.bmi})!)`
-  )
-} else if (mark.bmi > john.bmi) {
-  console.log(
-    `${mark.fullName}'s BMI(${mark.bmi} is higer than ${john.fullName}'s (${john.bmi})!)`
-  )
-}
-
-// console.log('Lifting weghts reperirion 0 (⌐■_■)')
-// console.log('Lifting weghts reperirion 1 (⌐■_■)')
-// console.log('Lifting weghts reperirion 2 (⌐■_■)')
-// console.log('Lifting weghts reperirion 3 (⌐■_■)')
-// console.log('Lifting weghts reperirion 4 (⌐■_■)')
-// console.log('Lifting weghts reperirion 5 (⌐■_■)')
-// console.log('Lifting weghts reperirion 6 (⌐■_■)')
-// console.log('Lifting weghts reperirion 7 (⌐■_■)')
-// console.log('Lifting weghts reperirion 8 (⌐■_■)')
-// console.log('Lifting weghts reperirion 9 (⌐■_■)')
-
-// for loop keeps running while condition is TRUE
-for (let rep = 0; rep < 10; rep++) {
-  console.log(`Lifting weghts reperirion ${rep} (⌐■_■)`)
-}
-
-const jonas = [
-  'Jonas',
-  'Schmedtmann',
-  2037 - 1991,
-  'teacher',
-  ['Michael', 'Steven', 'Peter'],
-  true,
-]
-
-const types = []
-
-for (let i = 0; i < jonas.length; i++) {
-  // reading from jonas array
-  console.log(jonas[i])
-
-  // filling types array
-  // types[i] = typeof jonas[i]
-  types.push(typeof jonas[i])
-}
-
-console.log(types)
-
-const years = [1991, 2007, 1969, 2020]
-const ages = []
-
-for (let i = 0; i < years.length; i++) {
-  ages.push(2037 - years[i])
-}
-console.log(ages)
-
-// continue and break
-console.log('--- ONLY STRINGS ---')
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] !== 'string') continue
-  console.log(jonas[i])
-}
-
-console.log('--- BREAK WITH NUMBER ---')
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] === 'number') break
-  console.log(jonas[i])
-}
-
-
-const jonas = [
-  'Jonas',
-  'Schmedtmann',
-  2037 - 1991,
-  'teacher',
-  ['Michael', 'Steven', 'Peter'],
-]
-
-for (let i = jonas.length - 1; i >= 0; i--) {
-  console.log(jonas[i])
-}
-
-for (let exercise = 1; exercise < 4; exercise++) {
-  console.log(`-- Starting exercise ${exercise}`)
-  for (let rep = 1; rep < 6; rep++) {
-    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} ^_^`)
+    // C) FIX
+    // value: Number(prompt('Degrees celsius:'))
+    value: 10,
   }
+
+  // B) FIND
+  // console.table(measurement)
+  // console.log(measurement.value)
+  // console.warn(measurement.value)
+  // console.error(measurement.value)
+
+  // const kelvin = Number(measurement.value) + 273
+  const kelvin = measurement.value + 273
+  return kelvin
 }
+// A) IDENTIFY
+console.log(measureKelvin())
 
+// Using a debugger
+const calsTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2)
+  console.log(temps)
 
-for (let rep = 1; rep < 10; rep++) {
-  // console.log(`Lifting weghts reperirion ${rep} (⌐■_■)`)
+  let max = 0
+  let min = 0
+  for (let i = 1; i < temps.length; i++) {
+    const curTemp = temps[i]
+    if (typeof curTemp !== 'number') continue
+    if (curTemp > max) max = curTemp
+    if (curTemp < min) min = curTemp
+  }
+  console.log(max, min)
+  return max - min
 }
+const amplitudeNew = calsTempAmplitudeBug([3, 5, 1], [9, 0, 5])
 
-let rep = 1
-while (rep <= 10) {
-  // console.log(`Lifting weghts reperirion ${rep} (⌐■_■)`)
-  rep++
-}
-
-let dice = Math.trunc(Math.random() * 6) + 1
-// console.log(dice)
-
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`)
-  dice = Math.trunc(Math.random() * 6) + 1
-  if (dice === 6) console.log('Loop is about to end...')
-}
+// A) IDENTIFY
+console.log(amplitudeNew)
 
 */
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
-const tips = []
-const totals = []
-function calcTip(bill) {
-  return bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill
-}
-for (let i = 0; i < bills.length; i++) {
-  tips.push(calcTip(bills[i]))
-  totals.push(tips[i] + bills[i])
-}
 
-console.log(bills)
-console.log(tips)
-console.log(totals)
-
-function calcAverage(arr) {
-  let sum = 0
+function printForecasat(arr) {
+  let temp = '...'
   for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i]
+    temp += ` ${arr[i]}℃ in ${i + 1} days ...`
   }
-  return sum / arr.length
+  console.log(temp)
 }
-let a = calcAverage(bills)
-let b = calcAverage(tips)
-let c = calcAverage(totals)
+const date1 = [17, 21, 23]
+const date2 = [12, 5, -5, 0, 4]
 
-console.log(a, b, c, a + b - c)
+printForecasat(date1)
+printForecasat(date2)
