50a9000e9e41e50226284ec56121e29b283ded54
zweidmitr <zweidmitr@gmail.com>
2022-09-13 08:25:23 +0300

    Looping Objects Keys and Entries

--- script.js
+++ script.js
@@ -90,7 +90,60 @@ const restaurant = {
     console.log(otherIngredients)
   },
 }
+
+// propery NAMES
+const properties = Object.keys(openingHours)
+// console.log(properties)
+
+let openStr = `We are open on ${properties.length} days: `
+
+for (const day of properties) {
+  openStr += `${day} `
+}
+// console.log(openStr)
+
+// property VALUES
+const values = Object.values(openingHours)
+// console.log(values)
+
+// Entire object
+const entries = Object.entries(openingHours)
+// console.log(entries)
+
+// [key, value]
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`)
+}
 /*
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open)
+
+// console.log(restaurant.openingHours.mon.open)
+
+// WIRH optional chaining
+console.log(restaurant.openingHours.mon?.open)
+console.log(restaurant.openingHours?.mon?.open)
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed'
+  console.log(`On ${day}, we open at ${open}`)
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist')
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist')
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@Jonas.io' }]
+const usersTest = []
+console.log(usersTest[0]?.name ?? 'User array empty')
+
+if (usersTest.length > 0) console.log(usersTest[0]?.name)
+else console.log('user array is empty')
+
+
 restaurant.orderDelivery({
   time: '22:30',
   address: 'Via del Sole, 21',
@@ -363,7 +416,6 @@ console.log(...game.scored)
 team1 < team2 && console.log('Team 1 is more likely to win')
 team1 > team2 && console.log('Team 2 is more likely to win')
 
-*/
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu]
 
@@ -374,3 +426,4 @@ for (const [i, el] of menu.entries()) {
 }
 
 // console.log([...menu.entries()])
+*/
