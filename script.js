2fe74d3de3468acb25870f8dd5a42c7f9aa39acc
zweidmitr <zweidmitr@gmail.com>
2022-09-14 22:04:38 +0300

    feat: Working with String

--- script.js
+++ script.js
@@ -91,6 +91,162 @@ const restaurant = {
   },
 }
 
+// Split and Join
+console.log('a+very+nice+string'.split('+'))
+console.log('Jonas Schmedtmann'.split(' '))
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ')
+console.log(firstName, lastName)
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ')
+console.log(newName)
+
+const capitalizeName = function (name) {
+  const names = name.split(' ')
+  const namesUpper = []
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1))
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()))
+  }
+  console.log(namesUpper.join(' '))
+}
+
+capitalizeName('jessica ann smith davis')
+capitalizeName('jonas schmedtmann')
+capitalizeName('dmitry alex')
+
+// Padding
+const message = 'Go to gate 23!'
+console.log(message.padStart(20, '+').padEnd(35, '+'))
+console.log('Jonas'.padStart(20, '+').padEnd(35, '+'))
+
+const maskCreditCard = function (number) {
+  const str = number + ''
+  const last = str.slice(-4)
+  return last.padStart(str.length, '*')
+}
+
+console.log(maskCreditCard(64637836))
+console.log(maskCreditCard(43378463864647384))
+console.log(maskCreditCard('334859493847755774747'))
+
+// Repeat
+const message2 = 'Bad weather... All Departues Delayed... '
+console.log(message2.repeat(5))
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'💫'.repeat(n)}`)
+}
+
+planesInLine(5)
+planesInLine(3)
+planesInLine(12)
+
+/*
+// Working with Strings - Part 2
+
+const airline = 'TAP Air Portugal'
+
+console.log(airline.toLowerCase())
+console.log(airline.toUpperCase())
+
+// Fix capitalization in name
+const passenger = 'jOnAS' //Jonas
+const passengerLower = passenger.toLowerCase()
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1)
+console.log(passengerCorrect)
+
+// Comparing emails
+
+const email = 'hello@jonas.io'
+const loginEmail = ' Hello@Jonas.Io \n'
+
+// const lowerEmail = loginEmail.toLowerCase()
+// const trimmedEmail = lowerEmail.trim()
+// console.log(trimmedEmail)
+
+const normalizedEmail = loginEmail.toLowerCase().trim()
+console.log(normalizedEmail)
+console.log(email === normalizedEmail)
+
+// replacing
+const priceGB = '288,78#'
+const priceUS = priceGB.replace('#', '$').replace(',', '.')
+console.log(priceUS)
+
+const announcement = 'All passengers come to barding door 23. Boarding door 23!'
+
+// console.log(announcement.replace('door', 'gate'))
+// console.log(announcement.replaceAll('door', 'gate'))
+console.log(announcement.replace(/door/g, 'gate'))
+
+// booleans
+const plane = 'Airbus A320neo'
+console.log(plane.includes('A320'))
+console.log(plane.includes('Boeing'))
+console.log(plane.startsWith('Airb'))
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family')
+}
+console.log('---------------------------------')
+// Practice exercise
+
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase()
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('you are NOT allowed on board')
+  } else {
+    console.log('Welcome aboard')
+  }
+}
+checkBaggage('I have a laptop, some Food and a pocket Knife')
+checkBaggage('Socks and camera')
+checkBaggage('Got some snacks and a gun for protection')
+
+
+// Working with Strings - Part 1
+const airline = 'TAP Air Portugal'
+const plane = 'A320'
+
+console.log(plane[0])
+console.log(plane[1])
+console.log(plane[2])
+console.log('B737'[0])
+
+console.log(airline.length)
+console.log('B737'.length)
+
+console.log(airline.indexOf('r'))
+console.log(airline.lastIndexOf('r'))
+console.log(airline.indexOf('portugal'))
+
+console.log(airline.slice(4))
+console.log(airline.slice(4, 7))
+
+console.log(airline.slice(0, airline.indexOf(' ')))
+console.log(airline.slice(airline.lastIndexOf(' ' + 1)))
+
+console.log(airline.slice(-2))
+console.log(airline.slice(1, -1))
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1)
+  if (s === 'B' || s === 'E') console.log('You got the middle seat')
+  else console.log('you got lucky')
+}
+
+checkMiddleSeat('11B')
+checkMiddleSeat('23C')
+checkMiddleSeat('3E')
+
+console.log(new String('jonas'))
+console.log(typeof new String('jonas'))
+console.log(typeof new String('jonas').slice(1))
+
+
 const gameEvents = new Map([
   [17, '⚽ GOAL'],
   [36, '� Substitution'],
@@ -128,7 +284,7 @@ for (const [key, value] of gameEvents) {
   console.log(`[${half} HALF] ${key} : ${value}`)
 }
 
-/*
+
 const question = new Map([
   ['question', 'what is the best programming language in the world?'],
   [1, 'C'],
